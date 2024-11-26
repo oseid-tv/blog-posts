7266a56039c94ed12a74a494c6d95b6e46bd90b0
Super Dev <superdev1031@gmail.com>
2024-11-25 19:53:42 -0800

    create content

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div className='ui comments'>
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img src="" alt='profile picture' />
+                </a>
+                <div className='content'>
+                    <a href='/' className='author'>
+                        Sarah
+                    </a>
+                    <div className='metadata'>
+                        <span className='date'>
+                            Today at 5:00PM
+                        </span>
+                    </div>
+                    <div className='text'>
+                        It's amazing
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
