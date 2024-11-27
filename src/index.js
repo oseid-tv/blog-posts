3a92d3a5d74f6132cb0c1f79464907020b66c705
Super Dev <superdev1031@gmail.com>
2024-11-26 20:04:26 -0800

    using props children

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import profile1 from './images/pic-1.jpg'
 import profile2 from './images/pic-2.jpg'
 import profile3 from './images/pic-3.jpg'
 import SingleComment from './SingleComment'
+import UserCard from './UserCard'
 
 const App = () => {
     return (
         <div className='ui comments'>
-            <SingleComment name='Alex' date='Today at 5:00 PM' text='It is amazing' picture={profile1} />
-            <SingleComment name='Jack' date='Today at 6:00 PM' text='Great job' picture={profile2} />
-            <SingleComment name='Sarah' date='Today at 7:00 PM' text='Thanks...' picture={profile3} />
+            <UserCard>
+                <div>
+                    Hello my name is Sarah, and I live in Caracas.
+                </div>
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Alex' date='Today at 5:00 PM' text='It is amazing' picture={profile1} />
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Jack' date='Today at 6:00 PM' text='Great job ' picture={profile2} />
+            </UserCard>
+            <UserCard>
+                <SingleComment name='Sarah' date='Today at 7:00 PM' text='Thanks...' picture={profile3} />
+            </UserCard>
         </div>
     )
 }
