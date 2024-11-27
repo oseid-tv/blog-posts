7f2d09b28b0640af7c6681f6a74272498f7e0166
Super Dev <superdev1031@gmail.com>
2024-11-26 19:38:57 -0800

    passing props to child

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
+import profile1 from './images/pic-1.jpg'
+import profile2 from './images/pic-2.jpg'
+import profile3 from './images/pic-3.jpg'
+import SingleComment from './SingleComment'
 
 const App = () => {
     return (
         <div className='ui comments'>
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img src="" alt='profile picture' />
-                </a>
-                <div className='content'>
-                    <a href='/' className='author'>
-                        Sarah
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>
-                            Today at 5:00PM
-                        </span>
-                    </div>
-                    <div className='text'>
-                        It's amazing
-                    </div>
-                </div>
-            </div>
+            <SingleComment name='Alex' date='Today at 5:00 PM' text='It is amazing' picture={profile1} />
+            <SingleComment name='Jack' date='Today at 6:00 PM' text='Great job' picture={profile2} />
+            <SingleComment name='Sarah' date='Today at 7:00 PM' text='Thanks...' picture={profile3} />
         </div>
     )
 }
